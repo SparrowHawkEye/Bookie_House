483587eb55e9db718f4f6d23b064f24a1e7c5e28
SparrowHawkEye <makh0043@gmail.com>
2022-05-04 14:38:45 +0600

    firebase Implemented

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Blogs from "./components/pages/Blogs/Blogs";
 import Home from "./components/pages/Home/Home";
 import NotFound from "./components/pages/NotFound/NotFound";
 import WareNav from "./components/Shared/WareNav/WareNav";
+import auth from "./firebase.init";
 
 function App() {
+  console.log(auth.config);
   return (
     <>
     <WareNav/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/inventory" element={<Blogs />} />
       <Route path="/blogs" element={<Blogs />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
